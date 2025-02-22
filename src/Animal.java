07cac6973e2af38990876f4e5f26885013e990f6
Jeremiah <JeremiahReimers@gmail.com>
2025-02-21 19:47:36 -0600

    changes

--- src/Animal.java
+++ src/Animal.java
@@ -1,10 +1,5 @@
 public interface Animal {
 
-    //constants
-    String scientificName = "";
-    double height = 0.0;
-    double weight = 0.0;
-
     //Methods
     void sleep();
     void eat();
